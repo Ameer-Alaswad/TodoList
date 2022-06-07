4a480f526fd5c14bf0bea44dbbbd62ffc8e7516c
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-06-07 17:05:08 +0200

    project working

--- app.js
+++ app.js
@@ -10,15 +10,26 @@ const listGenerator = (value) => {
 submit.addEventListener("click", () => {
   let list = document.createElement("li");
   let text = document.createTextNode(textInput.value);
+  // let span = document.createElement("span");
   const DeleteButton = document.createElement("button");
-  const checkbox = `<input id="checked" type="checkbox">`;
   list.appendChild(text);
   DeleteButton.innerHTML = "delete";
-  list.innerHTML += checkbox;
+  var checkbox = document.createElement("INPUT");
+  checkbox.setAttribute("type", "checkbox");
+  list.appendChild(checkbox);
+  const checked = document.querySelector("#checked");
   list.appendChild(DeleteButton);
   todosContainer.appendChild(list);
   DeleteButton.addEventListener("click", () => list.remove());
-  const checked = document.querySelector("#checked");
-  console.log(checked);
+
+  checkbox.addEventListener("click", (e) => {
+    const listText = list.innerHTML;
+    let checkboxState = e.target.checked;
+    if (checkboxState) {
+      list.style.textDecoration = "line-through";
+    } else {
+      list.style.textDecoration = "none";
+    }
+  });
   textInput.value = "";
 });
