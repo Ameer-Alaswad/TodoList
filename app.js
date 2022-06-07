135dc45f286ecc006e9d1dff82b7d7b5db23a436
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-06-07 21:27:21 +0200

    project primarly ready

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
 const todosContainer = document.querySelector("ul");
-const submit = document.querySelector("#submit");
+const submit = document.querySelector("form");
 const textInput = document.querySelector("#textInput");
+const addAtodo = document.querySelector("#addAtodo");
 
-const listGenerator = (value) => {
-  return `<li class=list>${value}<input type="checkbox"
-    id=checked></li>`;
-};
+submit.addEventListener("submit", (e) => {
+  if (textInput.value === "") {
+    e.preventDefault();
+    textInput.setCustomValidity("Plz add a Todo!");
+  } else {
+    e.preventDefault();
+    addAtodo.textContent = "";
 
-submit.addEventListener("click", () => {
-  let list = document.createElement("li");
-  let text = document.createTextNode(textInput.value);
-  // let span = document.createElement("span");
-  const DeleteButton = document.createElement("button");
-  list.appendChild(text);
-  DeleteButton.innerHTML = "delete";
-  var checkbox = document.createElement("INPUT");
-  checkbox.setAttribute("type", "checkbox");
-  list.appendChild(checkbox);
-  const checked = document.querySelector("#checked");
-  list.appendChild(DeleteButton);
-  todosContainer.appendChild(list);
-  DeleteButton.addEventListener("click", () => list.remove());
-
-  checkbox.addEventListener("click", (e) => {
-    const listText = list.innerHTML;
-    let checkboxState = e.target.checked;
-    if (checkboxState) {
-      list.style.textDecoration = "line-through";
-    } else {
-      list.style.textDecoration = "none";
-    }
-  });
-  textInput.value = "";
+    let list = document.createElement("li");
+    let text = document.createTextNode(textInput.value);
+    let span = document.createElement("span");
+    let deleteCheckboxContainer = document.createElement("span");
+    span.appendChild(text);
+    const DeleteButton = document.createElement("button");
+    list.appendChild(span);
+    DeleteButton.innerHTML = "X";
+    var checkbox = document.createElement("INPUT");
+    checkbox.setAttribute("type", "checkbox");
+    deleteCheckboxContainer.appendChild(checkbox);
+    deleteCheckboxContainer.appendChild(DeleteButton);
+    list.appendChild(deleteCheckboxContainer);
+    todosContainer.appendChild(list);
+    list.classList.add("lis");
+    span.classList.add("spanStyle");
+    deleteCheckboxContainer.classList.add("deleteCheckboxContainer");
+    DeleteButton.classList.add("deleteButton");
+    checkbox.classList.add("checkbox");
+    DeleteButton.addEventListener("click", () => list.remove());
+    checkbox.addEventListener("click", (e) => {
+      const listText = list.innerHTML;
+      let checkboxState = e.target.checked;
+      if (checkboxState) {
+        span.style.opacity = "0.5";
+        span.style.textDecoration = "line-through";
+      } else {
+        span.style.opacity = "1";
+        span.style.textDecoration = "none";
+      }
+    });
+    textInput.value = "";
+  }
 });
